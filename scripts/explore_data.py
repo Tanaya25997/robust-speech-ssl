caf5987fd6d5ce82432796c932be29a23b0e2b0e
Tanaya Atmaram Kambli <tatmaram@crusoe.ai>
2026-08-01 15:29:16 -0700

    feat: modify explore_date to write to experiments/analysis

--- scripts/explore_data.py
+++ scripts/explore_data.py
@@ -2,6 +2,10 @@ import torchaudio
 import os 
 import statistics
 
+output_dir = 'experiments/analysis'
+os.makedirs(output_dir, exist_ok=True)
+output_file = os.path.join(output_dir, "data_stats.txt")
+
 
 data_dir = '/data/LibriSpeech/train-clean-500'
 
@@ -27,11 +31,16 @@ for f in files[:500]:
 ## descriptive stats 
 
 
-print(f"Sample rates found: {set(sample_rates)}")
-print(f"Min duration:  {min(durations):.2f}s")
-print(f"Max duration:  {max(durations):.2f}s")
-print(f"Mean duration: {sum(durations)/len(durations):.2f}s")
-print(f"Files under 3s: {sum(1 for d in durations if d < 3)}")
-print(f"Files over 3s:  {sum(1 for d in durations if d >= 3)}")
-print(f"Median duration: {statistics.median(durations):.2f}s")
-print(f"Std duration:    {statistics.stdev(durations):.2f}s")
+with open(output_file, w) as f:
+    f.write(f"Total files: {len(files)} \n")
+    f.write(f"Sample rates found: {set(sample_rates)}\n")
+    f.write(f"Min duration:  {min(durations):.2f}s\n")
+    f.write(f"Max duration:  {max(durations):.2f}s\n")
+    f.write(f"Mean duration: {sum(durations)/len(durations):.2f}s\n")
+    f.write(f"Median duration: {statistics.median(durations):.2f}s\n")
+    f.write(f"Std duration:    {statistics.stdev(durations):.2f}s\n")
+    f.write(f"Files under 3s: {sum(1 for d in durations if d < 3)}\n")
+    f.write(f"Files over 3s:  {sum(1 for d in durations if d >= 3)}\n")
+
+
+print(f"Stats saved to {output_file}")
